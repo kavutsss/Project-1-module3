2a873e11caf1612476eb4e16fdd2c3ee1bb01dcc
Erika Gwiyo <erika.gwiyo@student.moringaschool.com>
2026-05-07 15:12:22 +0300

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDtCgxh5f0wp8DLH6gCGWgzceZ4fytw9Is",
+  authDomain: "project-1-module3.firebaseapp.com",
+  projectId: "project-1-module3",
+  storageBucket: "project-1-module3.firebasestorage.app",
+  messagingSenderId: "867860692988",
+  appId: "1:867860692988:web:27ca5674ec219129b47c79",
+  measurementId: "G-J01D75XNE7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
